293dde268c4cfd053be20eba6f37b9e5cdc740ae
Ayousshhhh <aya.mostafa2003@outlook.com>
2023-12-14 21:48:54 +0200

    newww

--- MacAttack/Assets/scripts/Flyingbomb.cs
+++ MacAttack/Assets/scripts/Flyingbomb.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class Flyingbomb : EnemyController
 {
-    public float HorizontalSpeed = 1.0f;
-    public float VerticalSpeed = 1.0f;
-    public float Amplitude = 1.0f;
-    public float minY = 0.0f; // Set the minimum y position
-    public float maxY = 5.0f; // Set the maximum y position
+    public float VerticalSpeed;
+    public float Amplitude;
+    public float MinY; // Set the minimum y position
+    public float MaxY; // Set the maximum y position
 
     private Vector3 startPosition;
 
@@ -19,16 +18,28 @@ public class Flyingbomb : EnemyController
 
     void Update()
     {
-        // Horizontal movement
-        transform.Translate(Vector2.right * HorizontalSpeed * Time.deltaTime);
-
         // Vertical movement using a sine wave
         float verticalMovement = Mathf.Sin(Time.time * VerticalSpeed) * Amplitude;
         float newY = startPosition.y + verticalMovement;
 
         // Clamp the y position to stay within the specified range
-        newY = Mathf.Clamp(newY, minY, maxY);
+        newY = Mathf.Clamp(newY, MinY, MaxY);
 
         transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            FindObjectOfType<playerStat>().TakeDamage(damage);
+            FlipY(); // Reverse the vertical direction when hitting the player
+        }
+    }
+
+    // Reverse the vertical direction
+    public void FlipY()
+    {
+        VerticalSpeed = -VerticalSpeed;
     }
 }
