619f3e4d02569e46b32191fc375e29946ad68740
Bakr <maboubakr9@gmail.com>
2023-12-16 04:16:13 +0200

    Update Hamburglardialogue.cs

--- MacAttack/Assets/scripts/Hamburglardialogue.cs
+++ MacAttack/Assets/scripts/Hamburglardialogue.cs
@@ -28,14 +28,14 @@ public class Hamburglardialogue : MonoBehaviour
                                  "Ronald McDonald: Good! I just need help for collecting Big Mac ingredients. Can you help me?",
                                  "Hamburglar: Of course! You'll need bread, onion, beef, tomatoes, pickles, and lettuce. But be careful, okay?",
                                  "Ronald McDonald: Thanks! What do I need to watch out for?",
-                                 "Hamburglar: Pay attention to only collect the right ingredience – no mistakes. And avoid boiling oil and spoiled ketchup!",
+                                 "Hamburglar: Pay attention to only collect the right ingredience  no mistakes. And avoid boiling oil and spoiled ketchup!",
                                  "Ronald McDonald: Got it! Thanks for the warning, friend!"};
 
             dialogueManager.SetSentences(dialogue);
             dialogueManager.StartCoroutine(dialogueManager.TypeDialogue());
 
-            }
-
         }
+
     }
 }
+
