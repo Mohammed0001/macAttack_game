02ee54c0c7cf98a8ff14add184fde2d2117b6965
hamsamedhat <hamsa.medhat44@gmail.com>
2023-12-16 04:09:54 +0200

    hamsa

--- MacAttack/Assets/scripts/Hamburglardialogue.cs
+++ MacAttack/Assets/scripts/Hamburglardialogue.cs
@@ -8,6 +8,7 @@ public class Hamburglardialogue : MonoBehaviour
     public Dialogue dialogueManager;
     private object other;
 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +24,7 @@ public class Hamburglardialogue : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            string[] dialogue = {"Hamburglar: Hey Ronald, how's it going?",
+                string[] dialogue = {"Hamburglar: Hey Ronald, how's it going?",
                                  "Ronald McDonald: Good! I just need help for collecting Big Mac ingredients. Can you help me?",
                                  "Hamburglar: Of course! You'll need bread, onion, beef, tomatoes, pickles, and lettuce. But be careful, okay?",
                                  "Ronald McDonald: Thanks! What do I need to watch out for?",
@@ -32,6 +33,8 @@ public class Hamburglardialogue : MonoBehaviour
 
             dialogueManager.SetSentences(dialogue);
             dialogueManager.StartCoroutine(dialogueManager.TypeDialogue());
+
+            }
 
         }
     }
--- MacAttack/Assets/scripts/grimacedialogue.cs
+++ MacAttack/Assets/scripts/grimacedialogue.cs
@@ -7,6 +7,7 @@ public class grimacedialogue : MonoBehaviour
 {
     public Dialogue dialogueManager;
     private object other;
+    public bool alreadyPlayed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +24,9 @@ public class grimacedialogue : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            string[] dialogue = {
+            if (!alreadyPlayed)
+            {
+                string[] dialogue = {
                                  "Ronald McDonald: Hello Grimace! .. Can you help me to collect weapons for my battle?",
                                  "Grimace: Of course! ... How Can I help you?",
                                  "Ronald McDonald: Can you tell me what kind of weapons should I collect?",
@@ -32,9 +35,11 @@ public class grimacedialogue : MonoBehaviour
                                  "Ronald McDonald: Okay .. Thank you!"
             };
 
-            dialogueManager.SetSentences(dialogue);
-            dialogueManager.StartCoroutine(dialogueManager.TypeDialogue());
+                dialogueManager.SetSentences(dialogue);
+                dialogueManager.StartCoroutine(dialogueManager.TypeDialogue());
+                alreadyPlayed = true;
+            }
 
-        }
+            }
     }
 }
